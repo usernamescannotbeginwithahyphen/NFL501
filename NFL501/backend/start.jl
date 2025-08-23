995122eb2891341e0e3ddcb4f889679371ba4b2c
usernamescannotbeginwithahyphen <jamesanselwright@gmail.com>
2025-08-22 19:14:49 -0500

    Update start.jl

--- NFL501/backend/start.jl
+++ NFL501/backend/start.jl
@@ -1,19 +1,4 @@
-import Pkg
-
-# Add the General registry once (no-op if it already exists)
-try
-    Pkg.Registry.add("General")
-catch err
-    @info "registry add skipped" err=err
-end
-
-# Resolve packages into the persistent depot
-Pkg.instantiate()
-
-# Start the Genie server on the port Render provides
 using Genie
 Genie.config.server_host = "0.0.0.0"
 Genie.config.server_port = parse(Int, get(ENV, "PORT", "8000"))
-
-# Boot your app
 include("/app/backend/config/routes.jl")
